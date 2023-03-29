1dcd3d1c1370eee7bd8dbf82a7be29e275f79206
Alexander Guthmann <4l3x83we.dev@gmail.com>
2023-03-29 21:19:50 +0200

    composer update and install permission und eloquent search

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Copyright (c) Alexander Guthmann.
+ *
+ * File: UserController.php
+ * User: ${USER}
+ * Date: 29.${MONTH_NAME_FULL}.2023
+ * Time: 20:17
+ */
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(User $user)
+    {
+    }
+
+    public function edit(User $user)
+    {
+    }
+
+    public function update(Request $request, User $user)
+    {
+    }
+
+    public function destroy(User $user)
+    {
+    }
+}
